4ce12e2b06ec1ef8e31fe604efea62a599afb177
ngochuyduong <huy.duong@saigontechnology.com>
2023-03-09 17:39:08 +0700

    fix: fix android cant run when downgrade react-native version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,30 +1,30 @@
-import * as React from 'react';
+// import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
-import { C, B, Button } from 'react-native-awesome-library3';
+// import { StyleSheet, View } from 'react-native';
+// import { C, B, Button } from 'react-native-awesome-library3';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <C />
-      <B />
-      <Button text="dd" onPress={() => null} />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <C />
+//       <B />
+//       <Button text="dd" onPress={() => null} />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 60,
+//     height: 60,
+//     marginVertical: 20,
+//   },
+// });
 
-// import StorybookUIRoot from '../.ondevice/Storybook';
-// export { StorybookUIRoot as default };
+import StorybookUIRoot from '../.ondevice/Storybook';
+export { StorybookUIRoot as default };
